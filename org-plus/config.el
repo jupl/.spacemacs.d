0e69450cdebb823ce7edf35e9d406a0852675338
jupl <juan.placencia.512@gmail.com>
2016-11-07 19:21:50 -0600

    Move org html CSS theme into external files

---
+++ org-plus/config.el
@@ -0,0 +1,2 @@
+(defvar org-plus-path (file-name-directory load-file-name)
+  "Base path for reading assets for this layer.")
--- org-plus/funcs.el
+++ org-plus/funcs.el
@@ -3,19 +3,22 @@
   (let* ((inline (eq org-html-htmlize-output-type 'inline-css))
          (background-color (if inline (face-background 'default) 'unset))
          (color (if inline (face-foreground 'default) 'unset)))
-    (format "<style type=\"text/css\">
-  html { font-family: sans-serif; }
-  pre.src { background-color: %s; color: %s; }
-  .figure img {
-    display: block;
-    max-width: 100%%;
-    margin: 0 auto;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.4);
-  }
-  @media screen { pre.src { overflow: auto; } }
-  @media print { pre.src { word-wrap: break-word; }}
-</style>
-" background-color color)))
+    (format (org-plus/css-template)
+            (org-plus/css-base-theme)
+            background-color
+            color)))
+
+(defun org-plus/css-base-theme ()
+  "Read base CSS theme from the filesystem."
+  (with-temp-buffer
+    (insert-file-contents (concat org-plus-path "theme.css"))
+    (buffer-string)))
+
+(defun org-plus/css-template ()
+  "Read CSS template from the filesystem."
+  (with-temp-buffer
+    (insert-file-contents (concat org-plus-path "theme.html"))
+    (buffer-string)))
 
 (defun org-plus/inline-css-hook (exporter)
   "If EXPORTER is html, add html customizations."
