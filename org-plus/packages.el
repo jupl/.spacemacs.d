a23c17891d2167f0f7ee38f4871ca04c6fcc662d
jupl <juan.placencia.512@gmail.com>
2016-11-21 22:36:23 -0600

    Clean up org package setup

--- org-plus/packages.el
+++ org-plus/packages.el
@@ -1,10 +1,9 @@
 (setq org-plus-packages '(org org-present))
 
-(defun org-plus/pre-init-org ()
+(defun org-plus/post-init-org ()
   "Setup to customize org stuff."
+  (spacemacs|disable-company org-mode)
   (spacemacs|use-package-add-hook org
-    :post-init
-    (spacemacs|disable-company org-mode)
     :post-config
     (progn
       (setq-default
@@ -12,28 +11,30 @@
        org-export-with-section-numbers nil
        org-export-with-toc nil
        org-export-allow-bind-keywords t
-       org-html-head-include-scripts nil
-       org-html-postamble nil
-       org-html-style-include-default nil
-       org-latex-compiler "xelatex"
-       org-latex-listings 'minted
        org-src-preserve-indentation t
        org-startup-folded 'showall)
-      (add-to-list 'org-agenda-files "~/org")
-      (add-to-list 'org-latex-packages-alist '("" "minted"))
-      (add-hook 'org-export-before-processing-hook
-                'org-plus/inline-css-hook))))
+      (add-to-list 'org-agenda-files "~/org")))
+  (with-eval-after-load 'ox-html
+    (setq-default
+     org-html-head-include-scripts nil
+     org-html-postamble nil
+     org-html-style-include-default nil)
+    (add-hook 'org-export-before-processing-hook
+              'org-plus/inline-css-hook))
+  (with-eval-after-load 'ox-latex
+    (setq-default
+     org-latex-compiler "xelatex"
+     org-latex-listings 'minted)
+    (add-to-list 'org-latex-packages-alist '("" "minted"))
+    (add-to-list 'org-latex-logfiles-extensions "pyg")))
 
-(defun org-plus/pre-init-org-present ()
+(defun org-plus/post-init-org-present ()
   "Setup to customize org-present."
-  (spacemacs|use-package-add-hook org-present
-    :post-init
-    (progn
-      (add-hook 'org-present-mode-hook 'org-plus/org-present-setup)
-      (add-hook 'org-present-mode-quit-hook 'org-plus/org-present-unsetup)
-      (evilified-state-evilify nil org-present-mode-keymap
-        (kbd "<prior>") 'org-present-prev
-        (kbd "<next>") 'org-present-next
-        "j" 'org-present-prev
-        "k" 'org-present-next
-        "." 'org-present-beginning))))
+  (add-hook 'org-present-mode-hook 'org-plus/org-present-setup)
+  (add-hook 'org-present-mode-quit-hook 'org-plus/org-present-unsetup)
+  (evilified-state-evilify nil org-present-mode-keymap
+    (kbd "<prior>") 'org-present-prev
+    (kbd "<next>") 'org-present-next
+    "j" 'org-present-prev
+    "k" 'org-present-next
+    "." 'org-present-beginning))
