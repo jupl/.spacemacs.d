aff1a22691077b11bd0cd831d5c80907e1a7d757
jupl <juan.placencia.512@gmail.com>
2017-01-19 10:02:57 -0600

    Add syntax highlighting for Brewfile

--- init.el
+++ init.el
@@ -197,10 +197,12 @@
   (add-to-list 'auto-mode-alist '("/\\.amethyst$" . json-mode))
   (add-to-list 'auto-mode-alist '("/\\.babelrc$" . json-mode))
   (add-to-list 'auto-mode-alist '("/\\.bowerrc$" . json-mode))
+  (add-to-list 'auto-mode-alist '("/\\.Brewfile$" . ruby-mode))
   (add-to-list 'auto-mode-alist '("/\\.eslintrc$" . json-mode))
   (add-to-list 'auto-mode-alist '("/\\.jsbeautifyrc$" . json-mode))
   (add-to-list 'auto-mode-alist '("/\\.jshintrc$" . json-mode))
   (add-to-list 'auto-mode-alist '("/\\.tern-project$" . json-mode))
+  (add-to-list 'auto-mode-alist '("/Brewfile$" . ruby-mode))
 
   ;; Additional keybinds
   (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
