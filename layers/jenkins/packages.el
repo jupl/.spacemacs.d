07abd11bd0f33026033ffc9c65fbab4d4ea39dc1
jupl <juan.placencia.512@gmail.com>
2017-08-30 20:04:50 -0500

    Add jenkins layer

---
+++ layers/jenkins/packages.el
@@ -0,0 +1,22 @@
+(setq jenkins-packages '(jenkins))
+
+(defun jenkins/init-jenkins ()
+  "Initialize Jenkins."
+  (use-package jenkins
+    :defer t
+    :config
+    (let ((common-keymap (copy-keymap evil-evilified-state-map)))
+      (define-key common-keymap "b" nil)
+      (define-key common-keymap "v" nil)
+      (let ((keymap (copy-keymap common-keymap)))
+        (define-key keymap "RET" nil)
+        (evilified-state-evilify-map jenkins-mode-map
+          :mode jenkins-mode
+          :evilified-map keymap))
+      (let ((keymap (copy-keymap common-keymap)))
+        (define-key keymap "1" nil)
+        (define-key keymap "g" nil)
+        (define-key keymap "$" nil)
+        (evilified-state-evilify-map jenkins-job-view-mode-map
+          :mode jenkins-job-view-mode
+          :evilified-map keymap)))))
