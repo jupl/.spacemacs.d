4671b27a01804af0a1bb7ebfd0830ddac743027b
jupl <juan.placencia.512@gmail.com>
2017-08-31 07:50:28 -0500

    [olivetti] Use progn

--- layers/olivetti/packages.el
+++ layers/olivetti/packages.el
@@ -5,15 +5,16 @@
   (use-package olivetti
     :defer t
     :init
-    (spacemacs|define-transient-state olivetti
-      :title "Olivetti"
-      :on-enter (turn-on-olivetti-mode)
-      :bindings
-      ("[" olivetti-shrink "Shrink")
-      ("]" olivetti-expand "Expand")
-      ("s" olivetti-set-width "Set width")
-      ("Q" olivetti-mode "Turn off" :exit t)
-      ("q" nil "Quit" :exit t))
-    (spacemacs/set-leader-keys "To" 'spacemacs/olivetti-transient-state/body)
+    (progn
+      (spacemacs|define-transient-state olivetti
+        :title "Olivetti"
+        :on-enter (turn-on-olivetti-mode)
+        :bindings
+        ("[" olivetti-shrink "Shrink")
+        ("]" olivetti-expand "Expand")
+        ("s" olivetti-set-width "Set width")
+        ("Q" olivetti-mode "Turn off" :exit t)
+        ("q" nil "Quit" :exit t))
+      (spacemacs/set-leader-keys "To" 'spacemacs/olivetti-transient-state/body))
     :config
     (spacemacs|hide-lighter olivetti-mode)))
